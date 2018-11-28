92d635df6ac9010f6af105c3fc8174f624688af8
Omar Idris <omaridris@tecace.com>
2018-11-27 17:43:52 -0800

    Using darker version of NavBar.jsx

--- react-client/src/Components/HeaderComponent/NavBar.jsx
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -5,27 +5,38 @@ import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 class NavBar extends Component {
   render() {
     return (
-        <Navbar>
+        <Navbar inverse collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
               <a href="/">Home</a>
             </Navbar.Brand>
+            <Navbar.Toggle />
           </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="/about">
-              About
-            </NavItem>
-            <NavItem eventKey={2} href="/contactUs">
-              Contact Us
-            </NavItem>
-            <NavDropdown eventKey={3} title="Dropdown (if necessary)" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.4}>Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="/about">
+                About
+              </NavItem>
+              <NavItem eventKey={2} href="/contactUs">
+                Contact Us
+              </NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                Link Right
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link Right
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
     )
   }
